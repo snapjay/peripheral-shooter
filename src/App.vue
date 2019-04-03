3a17d2064a0392b1a82ca26b63afb5363c374f29
Dan Shreim <dan@snapjay.com>
2019-04-03 18:09:58 -0400

    Add Layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+      <b-navbar toggleable="lg" type="dark" variant="primary">
+          <b-navbar-brand href="#">Peripheral Shooter</b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav>
+                  <b-nav-item to="/">Home</b-nav-item>
+                  <b-nav-item to="/about">About</b-nav-item>
+              </b-navbar-nav>
+          </b-collapse>
+      </b-navbar>
+    <b-container  id="app">
+      <b-row>
+        <b-col>
+          <router-view/>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Peripheral Shooter</h1>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
