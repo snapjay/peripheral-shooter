76696219542a00a08d9ee5cb95fb24e758d5bdd3
Dan Shreim <dan@snapjay.com>
2019-04-03 17:59:55 -0400

    Add Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+'use strict'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import './styles/styles.scss'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
