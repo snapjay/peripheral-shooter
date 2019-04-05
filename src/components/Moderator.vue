b7bfeccc539a59f28de7ad481a4fb3bd1ef8e436
Dan Shreim <dan@snapjay.com>
2019-04-05 14:05:14 -0400

    Make settings a Modal

--- src/components/Moderator.vue
+++ src/components/Moderator.vue
@@ -1,37 +1,41 @@
 <template>
     <div class="hello mb-3">
         <h1>Moderator</h1>
+        <b-modal id="settings" title="Settings">
+            <h6>Display Range</h6>
+            <b-row>
+                <b-col>
+                    <b-form-group label="From:">
+                        <b-form-input id="range-from" v-model="range.from" trim></b-form-input>
+                    </b-form-group>
+                </b-col>
+                <b-col>
+                    <b-form-group label="To:">
+                        <b-form-input id="range-to" v-model="range.to" trim></b-form-input>
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <hr>
+            <b-row>
+                <b-col>
+                    <b-form-group label="Update:" description="Frequency to display new content">
+                        <b-input-group append="ms">
+                            <b-form-input id="update" v-model="update" trim></b-form-input>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+                <b-col>
+                    <b-form-group label="Hide:" description="Hide content after this time">
+                        <b-input-group append="ms">
+                            <b-form-input id="hide" v-model="hide" trim></b-form-input>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+            </b-row>
+        </b-modal>
         <b-row>
             <b-col>
-                <b-form-group label="From">
-                    <b-form-input id="range-from" v-model="range.from" trim></b-form-input>
-                </b-form-group>
-            </b-col>
-            <b-col>
-                <b-form-group label="To">
-                    <b-form-input id="range-to" v-model="range.to" trim></b-form-input>
-                </b-form-group>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <b-form-group label="Update" description="Frequency to display new content">
-                    <b-input-group append="ms">
-                        <b-form-input id="update" v-model="update" trim></b-form-input>
-                    </b-input-group>
-                </b-form-group>
-            </b-col>
-            <b-col>
-                <b-form-group label="Show" description="How long to show content for">
-                    <b-input-group append="ms">
-                        <b-form-input id="hide" v-model="hide" trim></b-form-input>
-                    </b-input-group>
-                </b-form-group>
-            </b-col>
-        </b-row>
-
-        <b-row>
-            <b-col>
+                <b-button class="mr-2 float-right" variant="outline-primary" v-b-modal="'settings'" v-if="!running">Settings</b-button>
                 <b-button class="mr-2" variant="success" @click="start()" v-if="!running">Start</b-button>
                 <b-button variant="danger" @click="stop()" v-if="running">Stop</b-button>
             </b-col>
@@ -39,6 +43,7 @@
 
         <b-row>
             <b-col>
+                <h3 class="mt-3">Results</h3>
                 <b-table :items="rounds" :fields="fields">
                     <template slot="shot" slot-scope="data">
                         {{ rounds.length - data.index }}
@@ -67,7 +72,6 @@ export default {
         from: 1,
         to: 5,
       },
-      shots: 10,
       update: 2000,
       hide: 300,
     }
