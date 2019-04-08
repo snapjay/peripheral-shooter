6eaf9490db85adf32a4e3c3d127a848aec5da12e
Dan Shreim <dan@snapjay.com>
2019-04-08 14:14:22 -0400

    Add hasGameId prop; Hide in game menu items if there is no game
    Fixes #2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="h-100">
-      <b-navbar toggleable="lg" type="dark" variant="primary">
-          <b-navbar-brand :to="{name:'Home'}">Peripheral Shooter</b-navbar-brand>
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-          <b-collapse id="nav-collapse" is-nav>
-              <b-navbar-nav>
-                  <b-nav-item :to='{name: "Home"}'>Home</b-nav-item>
-                  <b-nav-item :to='{name: "Moderator"}'>Moderator</b-nav-item>
-                  <b-nav-item :to='{name: "Client", params:{view: "LEFT"}}'>Left</b-nav-item>
-                  <b-nav-item :to='{name: "Client", params:{view: "RIGHT"}}'>Right</b-nav-item>
-              </b-navbar-nav>
-          </b-collapse>
-      </b-navbar>
-    <b-container fluid class="h-100" id="app">
-      <b-row class="h-100">
-        <b-col class="h-100 p-0">
-          <router-view/>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+    <div class="h-100">
+        <b-navbar toggleable="lg" type="dark" variant="primary">
+            <b-navbar-brand :to="{name:'Home'}">Peripheral Shooter</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item :to='{name: "Home"}'>Home</b-nav-item>
+                    <b-nav-item v-if='hasGameId' :to='{name: "Moderator"}'>Moderator</b-nav-item>
+                    <b-nav-item v-if='hasGameId' :to='{name: "Client", params:{view: "LEFT"}}'>Left</b-nav-item>
+                    <b-nav-item v-if='hasGameId' :to='{name: "Client", params:{view: "RIGHT"}}'>Right</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <b-container fluid class="h-100" id="app">
+            <b-row class="h-100">
+                <b-col class="h-100 p-0">
+                    <router-view/>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
+<script>
+
+  export default {
+    name: 'app',
+    computed: {
+      hasGameId () {
+        return !!this.$route.params.gameId
+      },
+    },
+  }
+</script>
